ede4b79a45fbc5a15b33777108c2e8df79816dab
Bros <code.developer@gmx.de>
2017-12-02 13:49:34 +0100

    Initial commit

---
+++ desktop/src/ld/bros/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ld.bros.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ld.bros.game.LudumDare40;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LudumDare40(), config);
+	}
+}
